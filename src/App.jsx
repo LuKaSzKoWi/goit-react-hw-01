bdb1db506fdafd24d81766978fb47c9e5d87322b
LuKaSzKoWi <l.kowalski8509@gmail.com>
2024-10-28 21:34:38 +0100

    Task 3 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 
+// Task #1
+
+// import React from 'react';
+// import Profile from './components/Profile/Profile';
+// import userData from './userData.json';
+
+// const App = () => (
+//   <div>
+//     <Profile
+//       name={userData.username}
+//       tag={userData.tag}
+//       location={userData.location}
+//       image={userData.avatar}
+//       stats={userData.stats}
+//     />
+//   </div>
+// );
+
+// export default App;
 
 
-import React from 'react';
-import Profile from './components/Profile/Profile';
-import userData from './userData.json';
-
-const App = () => (
-  <div>
-    <Profile
-      name={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
-    />
-  </div>
-);
-
-export default App;
-
-
-
+// Task #2 
 
 // import React from 'react';
 // import FriendList from './components/FriendList/FriendList';
@@ -35,3 +35,20 @@ export default App;
 // };
 
 // export default App;
+
+
+// Task #3
+
+import React from 'react';
+import transactions from './transactions.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+const App = () => {
+  return (
+    <div>
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
+
+export default App;
