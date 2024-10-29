e0283ceb77b4f9ed8f4fb67dd329f39fa13c5861
LuKaSzKoWi <l.kowalski8509@gmail.com>
2024-10-29 16:00:01 +0100

    asimilate code in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-// Task #1
+
 
 // import React from 'react';
 // import Profile from './components/Profile/Profile';
@@ -20,7 +20,7 @@
 // export default App;
 
 
-// Task #2 
+
 
 // import React from 'react';
 // import FriendList from './components/FriendList/FriendList';
@@ -37,15 +37,43 @@
 // export default App;
 
 
-// Task #3
+
+
+// import React from 'react';
+// import transactions from './transactions.json';
+// import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <TransactionHistory items={transactions} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
 
 import React from 'react';
-import transactions from './transactions.json';
+import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 const App = () => {
   return (
     <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
   );
